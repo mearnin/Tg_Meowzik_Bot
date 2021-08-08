5c216f7c7729cbe343f0efdbfb82d0ed87e2c50f
mearnin <87330597+mearnin@users.noreply.github.com>
2021-08-08 14:01:40 +0530

    sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,7 +3,7 @@
 bot_token = 1905204525:AAGRVnvUx8piOwJ_8_pOFLUK7gWK7DEhQ8A
 sudo_chat_id = -1827688640
 
-ARQ_API_KEY = "Place your Key Here -ARQ" # Get it from @ARQRobot. Needed for Saavn and Deezer.
+ARQ_API_KEY = GKEKEP-XAUYLY-YASWEH-ZKDHEC-ARQ # Get it from @ARQRobot. Needed for Saavn and Deezer.
 DeezerQuality = 3 # 1 = 128k Audio Quality
                   # 3 = 320k Audio Quality
                   # 9 = FLAC Format Very High Quality !
