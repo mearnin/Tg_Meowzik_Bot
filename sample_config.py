786426657f114f07fe9223cb563fca8f164c1337
mearnin <87330597+mearnin@users.noreply.github.com>
2021-08-08 13:56:29 +0530

    sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 # Change all Values Here Before you use.....
 
-bot_token = "Your_Bot_Token_Here"
+bot_token = 1905204525:AAGRVnvUx8piOwJ_8_pOFLUK7gWK7DEhQ8A
 sudo_chat_id = -1234567777
 
 ARQ_API_KEY = "Place your Key Here -ARQ" # Get it from @ARQRobot. Needed for Saavn and Deezer.
